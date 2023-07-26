46a062567c5663458d303c98119c9bf7072ee9f0
king lich <iljapuskov@gmail.com>
2023-07-26 18:31:28 +0300

    feature: implement identity server

--- MmoWeb/Dal/Data/ApplicationContext.cs
+++ MmoWeb/Dal/Data/ApplicationContext.cs
@@ -1,9 +1,26 @@
 ﻿using Entities.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dal.Data;
 
-public class ApplicationContext : IdentityDbContext<User,>
+public class ApplicationContext : IdentityDbContext<User,
+    Role,
+    Guid,
+    IdentityUserClaim<Guid>,
+    IdentityUserRole<Guid>,
+    IdentityUserLogin<Guid>,
+    IdentityRoleClaim<Guid>,
+    IdentityUserToken<Guid>>
 {
-    
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
 }
--- MmoWeb/Identity.Web/Program.cs
+++ MmoWeb/Identity.Web/Program.cs
@@ -1,32 +1,84 @@
 
+using System.Reflection;
+using Dal.Data;
+using Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace Identity.Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var policyName = "allowAll";
 
-            // Add services to the container.
+            var builder = WebApplication.CreateBuilder(args);
+            
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(policyName,
+                    policy =>
+                    {
+                        policy
+                            .AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
 
-            // Configure the HTTP request pipeline.
+            var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
+            builder.Services.AddDbContext<ApplicationContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("entityDb"),
+                    migration => migration.MigrationsAssembly(migrationAssembly)));
+
+            builder.Services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddRoleManager<RoleManager<Role>>()
+                .AddUserManager<UserManager<User>>()
+                .AddDefaultTokenProviders();
+
+
+            builder.Services.AddIdentityServer(options =>
+                {
+                    options.UserInteraction.LoginUrl = null;
+                })
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = context =>
+                        context.UseNpgsql(builder.Configuration.GetConnectionString("configurationDb"),
+                            migration => migration.MigrationsAssembly(migrationAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = context =>
+                        context.UseNpgsql(builder.Configuration.GetConnectionString("operationalDb"),
+                            migration => migration.MigrationsAssembly(migrationAssembly));
+                })
+                .AddDeveloperSigningCredential()
+                .AddAspNetIdentity<User>();
+
+            var app = builder.Build();
+            
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(policyName);
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
+            app.UseIdentityServer();
 
             app.MapControllers();
 
