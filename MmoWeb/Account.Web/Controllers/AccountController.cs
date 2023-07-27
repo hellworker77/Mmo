7550a040c0ee7f818991784d0ad21bf3cccc927c
king lich <iljapuskov@gmail.com>
2023-07-28 01:16:25 +0300

    feature: configure swagger and auth in account

---
+++ MmoWeb/Account.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Common.IServices;
+using Common.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IIdentityService _identityService;
+    private readonly IAccountService _accountService;
+
+    public AccountController(IIdentityService identityService,
+        IAccountService accountService)
+    {
+        _identityService = identityService;
+        _accountService = accountService;
+    }
+
+    [HttpGet("id")]
+    public async Task<UserDto> GetUserByIdAsync(Guid userId)
+    {
+        return await _accountService.GetByIdAsync(userId);
+    }
+}
