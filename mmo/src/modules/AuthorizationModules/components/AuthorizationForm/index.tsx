79732bb5993262f2cd6259ad74b234712a8852d1
VadimBestSorka <113136826+BrainHew@users.noreply.github.com>
2023-07-28 05:38:05 +0300

    feat: release authorization

--- mmo/src/modules/AuthorizationModules/components/AuthorizationForm/index.tsx
+++ mmo/src/modules/AuthorizationModules/components/AuthorizationForm/index.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AuthorizationForm: React.FC = () => {
-  return <div>АВТОРИЗАЦИЯ</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-64">
+        <input
+          className="mb-4 w-full rounded border p-2"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          className="mb-4 w-full rounded border p-2"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="Password"
+        />
+        <div className="flex justify-between">
+          <button className="rounded bg-blue-500 px-4 py-2 text-white">
+            Авторизация
+          </button>
+          <button className="rounded bg-gray-500 px-4 py-2 text-white">
+            Регистрация
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { AuthorizationForm };
--- mmo/tailwind.config.js
+++ mmo/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.ts",
-    "./**/*.html"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: {
+          100: '#FFFEF8',
+          200: '#FFFFEB',
+          300: '#f7f5f0',
+          400: '#ab8673',
+          500: '#d7b29f',
+          600: '#12121c'
+        },
+      }
+    },
   },
   plugins: [],
 }
